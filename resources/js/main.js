97f9abea2b10c341095da74534ff61db3943392d
DESKTOP-UK66V8F\User <full.of.october@gmail.com>
2023-03-01 17:48:29 +0200

    galery for order-page is in proccess

--- resources/js/main.js
+++ resources/js/main.js
@@ -62,6 +62,15 @@ $('.js-recommendations-gallery').slick({
   ]
 });
 
+
+$('.js-order-products-gallery').slick({
+  infinite: true,
+  slidesToShow: 3,
+  slidesToScroll: 3
+});
+
+
+
 // $(document).scroll(function (event) {
 //   // document.querySelector("body") === $("body")
 //   let scrollTop = $("body").scrollTop();
